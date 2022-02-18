651b11cef4936beb56b4aa9cf405525fdbf8d374
Djonatas Tenfen <djonatas@live.com>
2022-02-18 14:16:51 -0300

    Limpeza

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-var express = require('express');
-var app = express();
-//var http = require('http').Server(app);
-
+const express = require('express');
+const app = express();
 
 //var ffmpeg = require('fluent-ffmpeg');
 //var stream = require('stream');
@@ -10,39 +8,31 @@ var fs = require('fs');
 var https = require('https');
 app.use(express.static('public'));
 
-const server = require('http').createServer(//{
- //key: fs.readFileSync('abels-key.pem'),
- // cert: fs.readFileSync('abels-cert.pem')
- 
-	//},
-	app);
-
+const server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
-spawn('ffmpeg',['-h']).on('error',function(m){
-	console.error("FFMpeg not found in system cli; please install ffmpeg properly or make a softlink to ./!");
-	process.exit(-1);
+	spawn('ffmpeg',['-h']).on('error',function(m){
+		console.error("FFMpeg not found in system cli; please install ffmpeg properly or make a softlink to ./!");
+		process.exit(-1);
 });
 
 
 io.on('connection', function(socket){
 	socket.emit('message','Hello from mediarecorder-to-rtmp server!');
 	socket.emit('message','Please set rtmp destination before start streaming.');
-	
-	
-	
-	var ffmpeg_process, feedStream=false;
+
+	let ffmpeg_process, feedStream=false;
 	socket.on('config_rtmpDestination',function(m){
 		if(typeof m != 'string'){
 			socket.emit('fatal','rtmp destination setup error.');
 			return;
 		}
-		var regexValidator=/^rtmp:\/\/[^\s]*$/;//TODO: should read config
+		let regexValidator=/^rtmp:\/\/[^\s]*$/;//TODO: should read config
 		if(!regexValidator.test(m)){
 			socket.emit('fatal','rtmp address rejected.');
 			return;
 		}
-		socket._rtmpDestination=m;
+		socket._rtmpDestination = m;
 		socket.emit('message','rtmp destination set to:'+m);
 	}); 
 	//socket._vcodec='libvpx';//from firefox default encoder
@@ -54,14 +44,12 @@ io.on('connection', function(socket){
 		if(!/^[0-9a-z]{2,}$/.test(m)){
 			socket.emit('fatal','input codec contains illegal character?.');
 			return;
-		}//for safety
+		}
 		socket._vcodec=m;
 	}); 	
 
-
 	socket.on('start',function(m){
 		if(ffmpeg_process || feedStream){
-			
 			socket.emit('fatal','stream already started.');
 			return;
 		}
@@ -70,34 +58,32 @@ io.on('connection', function(socket){
 			return;
 		}
 		
-		var framerate = socket.handshake.query.framespersecond;
-		var audioBitrate = parseInt(socket.handshake.query.audioBitrate);
-	    var audioEncoding = "64k";
-		if (audioBitrate ==11025){
+		const framerate = socket.handshake.query.framespersecond;
+		const audioBitrate = parseInt(socket.handshake.query.audioBitrate);
+		let audioEncoding = "64k";
+		if (audioBitrate === 11025){
 			audioEncoding = "11k";
-		} else if (audioBitrate ==22050){
+		} else if (audioBitrate === 22050){
 			audioEncoding = "22k";
-		} else if (audioBitrate ==44100){
+		} else if (audioBitrate === 44100){
 			audioEncoding = "44k";
 		}
 		console.log(audioEncoding, audioBitrate);
 		console.log('framerate on node side', framerate);
-		//var ops = [];
-		if (framerate == 1){
-			var ops = [
+		let ops = []
+		if (framerate === 1){
+			ops = [
 				'-i','-',
-				 '-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency', 
-				//'-max_muxing_queue_size', '1000', 
-				//'-bufsize', '5000',
-			       '-r', '1', '-g', '2', '-keyint_min','2', 
+				 '-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency',
+			    '-r', '1', '-g', '2', '-keyint_min','2',
 					'-x264opts', 'keyint=2', '-crf', '25', '-pix_fmt', 'yuv420p',
-			        '-profile:v', 'baseline', '-level', '3', 
-     				'-c:a', 'aac', '-b:a', audioEncoding, '-ar', audioBitrate, 
-			        '-f', 'flv', socket._rtmpDestination		
+			    '-profile:v', 'baseline', '-level', '3',
+					'-c:a', 'aac', '-b:a', audioEncoding, '-ar', audioBitrate,
+					'-f', 'flv', socket._rtmpDestination
 			];
 			
-		}else if (framerate == 15){
-			var ops = [
+		} else if (framerate === 15){
+			ops = [
 				'-i','-',
 				 '-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency', 
 				'-max_muxing_queue_size', '1000', 
@@ -109,33 +95,35 @@ io.on('connection', function(socket){
 			        '-f', 'flv', socket._rtmpDestination		
 			];
 			
-		}else{
-		   var ops=[
+		} else {
+		   ops=[
 				 '-i','-',
 				'-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency',  // video codec config: low latency, adaptive bitrate
 				'-c:a', 'aac', '-ar', audioBitrate, '-b:a', audioEncoding, // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
 				'-bufsize', '5000', '-f', 'flv', socket._rtmpDestination
-		];
-	}
-	console.log("ops", ops);
+		];}
+
+		console.log("ops", ops);
 		console.log(socket._rtmpDestination);
+
 		ffmpeg_process=spawn('ffmpeg', ops);
 		console.log("ffmpeg spawned");
-		feedStream=function(data){
-			
+
+		feedStream = (data) => {
 			ffmpeg_process.stdin.write(data);
-			//write exception cannot be caught here.	
 		}
 
 		ffmpeg_process.stderr.on('data',function(d){
 			socket.emit('ffmpeg_stderr',''+d);
 		});
+
 		ffmpeg_process.on('error',function(e){
 			console.log('child process error'+e);
 			socket.emit('fatal','ffmpeg error!'+e);
 			feedStream=false;
 			socket.disconnect();
 		});
+
 		ffmpeg_process.on('exit',function(e){
 			console.log('child process exit'+e);
 			socket.emit('fatal','ffmpeg exit!'+e);
@@ -143,8 +131,8 @@ io.on('connection', function(socket){
 		});
 	});
 
-	socket.on('binarystream',function(m){
-		if(!feedStream){
+	socket.on('binarystream', (m) => {
+		if( !feedStream ){
 			socket.emit('fatal','rtmp not set yet.');
 			ffmpeg_process.stdin.end();
 			ffmpeg_process.kill('SIGINT');
@@ -152,7 +140,8 @@ io.on('connection', function(socket){
 		}
 		feedStream(m);
 	});
-	socket.on('disconnect', function () {
+
+	socket.on('disconnect', () => {
 		console.log("socket disconnected!");
 		feedStream=false;
 		if(ffmpeg_process)
@@ -162,8 +151,7 @@ io.on('connection', function(socket){
 			console.log("ffmpeg process ended!");
 		}catch(e){console.warn('killing ffmoeg process attempt failed...');}
 	});
-	socket.on('error',function(e){
-		
+	socket.on('error', (e) => {
 		console.log('socket.io error:'+e);
 	});
 });
@@ -172,13 +160,10 @@ io.on('error',function(e){
 	console.log('socket.io error:'+e);
 });
 
-
 server.listen(process.env.PORT || 1437, function(){
   console.log('https and websocket listening on *:1437');
 });
 
 process.on('uncaughtException', function(err) {
-    // handle the error safely
-    console.log(err)
-    // Note: after client disconnect, the subprocess will cause an Error EPIPE, which can only be caught this way.
+	console.log(err)
 })
